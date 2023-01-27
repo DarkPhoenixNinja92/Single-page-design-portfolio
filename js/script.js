e436d84ebdac82db75c499a3da1c7735354a1d46
Shaun <scpour@gmail.com>
2023-01-27 14:56:54 -0500

    Left and right btns work until user tries to alternate between them

--- js/script.js
+++ js/script.js
@@ -34,5 +34,29 @@ btnRight.addEventListener('click', () => {
         image1.classList.remove('hide');
         currentImg = 0;
     }
-})
+});
 
+btnLeft.addEventListener('click', () => {
+    
+    if(currentImg == 0) {
+        image1.classList.add('hide');
+        image5.classList.remove('hide');
+        currentImg++;
+    } else if(currentImg == 1) {
+        image5.classList.add('hide');
+        image4.classList.remove('hide');
+        currentImg++;
+    } else if(currentImg == 2) {
+        image4.classList.add('hide');
+        image3.classList.remove('hide');
+        currentImg++;
+    } else if(currentImg == 3) {
+        image3.classList.add('hide');
+        image2.classList.remove('hide');
+        currentImg++;
+    } else if(currentImg == 4) {
+        image2.classList.add('hide');
+        image1.classList.remove('hide');
+        currentImg = 0;
+    }
+});
